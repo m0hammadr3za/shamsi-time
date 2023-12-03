b83112627b9ce607615e368c10f9ad61fa1ad572
Farshad <m0hammadr3za.dev@gmail.com>
2023-12-03 20:54:48 +0330

    🔨 Update build script

--- build.bat
+++ build.bat
@@ -2,23 +2,25 @@
 REM Build the executable using pyinstaller
 pyinstaller --onefile --noconsole script.py
 
-REM Rename the .exe file
-rename dist\script.exe script.exe
-
-REM Create "Shamsi time" directory if it doesn't exist
-if not exist "Shamsi time" mkdir "Shamsi time"
-
-REM Move the script.exe file to the "Shamsi time" directory
-move dist\script.exe "Shamsi time"
-
-REM Copy the icons to the "Shamsi time" directory
-copy config.txt "Shamsi time"
-xcopy assets "Shamsi time\assets" /E /I
-
-REM Cleanup
+REM Delet build directory and script.spec
 rmdir /s /q build
-rmdir /s /q dist
 del script.spec
+
+REM Rename the .exe file
+rename dist\script.exe "Shamsi time".exe
+
+REM Create build directory if it doesn't exist
+if not exist build mkdir build
+
+REM Move the Shamsi time.exe file to the build directory
+move dist\"Shamsi time".exe build
+
+REM Copy the icons to the build directory
+copy config.txt build
+xcopy assets build\assets /E /I
+
+REM Delete dist file
+rmdir /s /q dist
 
 echo Build creation complete!
 pause
